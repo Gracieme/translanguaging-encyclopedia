7f0e6a20fe72890b018b46cfecf6d41b7d045eec
Gracie Jiaxin Shen <jiaxinshen1208@gmail.com>
2026-08-05 23:50:51 +1200

    Author praxis concept examples individually

--- app/manualContent/index.ts
+++ app/manualContent/index.ts
@@ -1,10 +1,12 @@
 import { boundariesContent } from "./boundaries.ts";
 import { foundationsContent } from "./foundations.ts";
 import { powerContent } from "./power.ts";
+import { praxisContent } from "./praxis.ts";
 import type { ManualConceptContent } from "./types.ts";
 
 export const manualConceptContent: Record<string, ManualConceptContent> = {
   ...foundationsContent,
   ...boundariesContent,
   ...powerContent,
+  ...praxisContent,
 };
