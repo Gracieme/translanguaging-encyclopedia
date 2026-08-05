71bd2e680dbb9ccb87762b0e5d8266ca819f247f
Gracie Jiaxin Shen <jiaxinshen1208@gmail.com>
2026-08-05 23:40:37 +1200

    Author boundary concept examples individually

---
+++ app/manualContent/index.ts
@@ -0,0 +1,6 @@
+import { boundariesContent } from "./boundaries.ts";
+import type { ManualConceptContent } from "./types.ts";
+
+export const manualConceptContent: Record<string, ManualConceptContent> = {
+  ...boundariesContent,
+};
