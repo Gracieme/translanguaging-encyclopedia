537d3348dd3ca7d72fdb43136dae4d24ab2006cd
Gracie Jiaxin Shen <jiaxinshen1208@gmail.com>
2026-08-05 23:58:22 +1200

    Author literacy concept examples individually

--- app/manualContent/index.ts
+++ app/manualContent/index.ts
@@ -4,6 +4,7 @@ import { powerContent } from "./power.ts";
 import { praxisContent } from "./praxis.ts";
 import { cognitionContent } from "./cognition.ts";
 import { educationContent } from "./education.ts";
+import { literacyContent } from "./literacy.ts";
 import type { ManualConceptContent } from "./types.ts";
 
 export const manualConceptContent: Record<string, ManualConceptContent> = {
@@ -13,4 +14,5 @@ export const manualConceptContent: Record<string, ManualConceptContent> = {
   ...praxisContent,
   ...cognitionContent,
   ...educationContent,
+  ...literacyContent,
 };
